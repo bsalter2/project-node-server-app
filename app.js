da7918c811fce3f1de9273a9b2f9619d52b08551
bsalter2 <brendansalter97@gmail.com>
2023-06-20 12:50:39 -0700

    added users db with backend

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from 'express'
 import session from "express-session";
 import cors from 'cors'
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
+import UserController from './controllers/users/users-controller.js';
+import AuthController from './controllers/users/auth-controller.js';
 
-dotenv.config()
+// dotenv.config()
 
 const app = express()
 app.use(
@@ -22,11 +24,14 @@ app.use(
 );
 
 app.use(express.json())
-
+UserController(app)
+AuthController(app)
 const port = process.env.PORT || 4000;
 app.listen(port)
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+const CONNECTION_STRING = "mongodb+srv://testUser:test@cluster0.tbpbyz4.mongodb.net/?retryWrites=true&w=majority"
+
 mongoose.connect(CONNECTION_STRING).then(
     () => { 
        console.log("Connected to DB!");
