6269b0537c8e19ee1c1f6e8a4d1229084b9dbbbb
Samuel Lee <sam@samuels-mbp.lan>
2023-06-23 11:56:02 -0700

    profile page

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-import express from 'express'
+import express from "express";
 import session from "express-session";
-import cors from 'cors'
+import cors from "cors";
 import mongoose from "mongoose";
 // import dotenv from "dotenv"
-import UserController from './controllers/users/users-controller.js';
-import AuthController from './controllers/users/auth-controller.js';
+import UserController from "./controllers/users/users-controller.js";
+import AuthController from "./controllers/users/auth-controller.js";
 
 // dotenv.config()
 
-const app = express()
+const app = express();
 app.use(
-    session({
-        secret: "any string",
-        resave: false,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 app.use(
-    cors({
-        credentials: true,
-        origin: '*'
-    })
+  cors({
+    credentials: true,
+    origin: "*",
+  })
 );
 
-app.use(express.json())
-UserController(app)
-AuthController(app)
+app.use(express.json());
+UserController(app);
+AuthController(app);
 const port = process.env.PORT || 4000;
-app.listen(port)
+app.listen(port);
 
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-const CONNECTION_STRING = "mongodb+srv://testUser:test@cluster0.tbpbyz4.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_STRING =
+  "mongodb+srv://testUser:test@cluster0.tbpbyz4.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(CONNECTION_STRING).then(
-    () => { 
-       console.log("Connected to DB!");
-   },
-    err => { 
-      console.log(err);
-   }
-  );
-
+  () => {
+    console.log("Connected to DB!");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
--- controllers/users/users-schema.js
+++ controllers/users/users-schema.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
-const usersSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: {type: String, required: true},
-  firstName: String,
-  lastName: String,
-}, { collection: "users" });
+const usersSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+  },
+  { collection: "users" }
+);
 export default usersSchema;
